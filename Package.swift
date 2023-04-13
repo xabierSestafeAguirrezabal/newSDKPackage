83d9773e1b8c273210f832823d4d0652db5312cf
Xabier Sestafe <xabier.sestafe@ironchip.com>
2023-04-13 09:13:53 +0200

    wip

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
                   name: "Ironchipauthenticatorgolangsdk",
-                  path: "Frameworks/Ironchipauthenticatorgolangsdk.framework.zip"
+                  path: "Framework/Ironchipauthenticatorgolangsdk.framework.zip"
               ),
     ]
 )
